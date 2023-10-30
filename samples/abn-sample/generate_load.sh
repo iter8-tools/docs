d3443c4233fc579b1c229756b224aa6f95511f5d
Michael Kalantar <kalantar@us.ibm.com>
2023-10-30 17:52:01 -0400

    update load test script
    Signed-off-by: Michael Kalantar <kalantar@us.ibm.com>

--- samples/abn-sample/generate_load.sh
+++ samples/abn-sample/generate_load.sh
@@ -8,13 +8,13 @@ BUY="$SERVICE/buy"
 while (( 1 )); do 
     __user=$(uuidgen)
     __num_purchases=$(( ( RANDOM % 5 ) +1 ))
-    j=0
-    while (( ${j} < ${__num_purchases} )); do
+    j=1
+    while (( ${j} <= ${__num_purchases} )); do
         echo "purchase $j of $__num_purchases for user $__user"
         __num_recommendations=$(( ( RANDOM % 5 )  + 1 ))
         # get some recommendations
-        i=0
-        while (( ${i} < ${__num_recommendations} )); do
+        i=1
+        while (( ${i} <= ${__num_recommendations} )); do
             echo "> recommendation $i of $__num_recommendations"
             curl -s ${GET_RECOMMENDATION} -H "X-User: ${__user}"
             sleep $(( ( RANDOM % 2000 ) / 1000 ))
